1676b45936a07498ee027c0f0ff047c4ffa1cdf2
shedinho20 <shedinhoshedrach@gmail.com>
2022-01-09 19:31:54 +0100

    logic for the home search moved to useSearch

--- src/component/Compounds/Search/index.tsx
+++ src/component/Compounds/Search/index.tsx
@@ -1,148 +1,26 @@
 import "./search.scss";
 import BedIcon from "@mui/icons-material/Bed";
-import { ChangeEvent, InputHTMLAttributes, useEffect, useRef, useState } from "react";
 import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
 import HouseSidingIcon from "@mui/icons-material/HouseSiding";
-import { useDispatch, useSelector } from "react-redux";
-import { Istate } from "../../../store";
-
 import { Datepicker } from "../../molecules/DatePicker";
-
-interface State {
-  locationFocus: boolean;
-  calenderCheckin: boolean;
-  calenderCheckOut: boolean;
-  adultInfo: boolean;
-  location: string;
-  checkIn: Date | null;
-  checkOut: Date | null;
-  adults: number;
-  room: number;
-}
+import { useSearch } from "./useSearch";
 
 export const Search = () => {
-  const dispatch = useDispatch();
-  const RefDet = useRef<HTMLHeadingElement>(null);
-  const searchBoxRef = useRef<any>(null);
-  const [state, setstate] = useState<State>({
-    locationFocus: false,
-    calenderCheckin: false,
-    calenderCheckOut: false,
-    adultInfo: false,
-    location: "",
-    checkIn: null,
-    checkOut: null,
-    adults: 2,
-    room: 1,
-  });
-  const { theme } = useSelector((state: Istate) => state);
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setstate({
-      ...state,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handlefocus = (e: ChangeEvent<HTMLInputElement>) => {
-    setstate({
-      ...state,
-      locationFocus: true,
-      calenderCheckin: false,
-      calenderCheckOut: false,
-    });
-  };
-  const handleBlur = (e: ChangeEvent<HTMLInputElement>) => {
-    setstate({
-      ...state,
-      locationFocus: false,
-    });
-  };
-
-  const handleClickOutside = (e: any) => {
-    if (e.target.className === "searchLocation" && state.adultInfo !== true) return;
-    if (e.target.className === "infoSection infoSectionOpen") return;
-    if (e.target.className === "MuiDialog-container MuiDialog-scrollPaper css-hz1bth-MuiDialog-container") {
-      setstate({
-        ...state,
-        calenderCheckin: false,
-        calenderCheckOut: false,
-      });
-    }
-    if (e.target.className === "infoSection") return;
-    if (state.calenderCheckin) return;
-
-    if (null !== RefDet.current) {
-      if (!RefDet.current.contains(e.target)) {
-        setstate({
-          ...state,
-          calenderCheckin: false,
-          adultInfo: false,
-          locationFocus: false,
-        });
-      }
-    }
-  };
-
-  const handleClickinside = (e: any) => {
-    setstate({
-      ...state,
-      calenderCheckin: false,
-      calenderCheckOut: false,
-      locationFocus: false,
-      adultInfo: !state.adultInfo,
-    });
-  };
-
-  const handleClickin = () => {
-    setstate({
-      ...state,
-      checkOut: null,
-      calenderCheckin: !state.calenderCheckin,
-      calenderCheckOut: false,
-    });
-  };
-  const handleClickout = () => {
-    setstate({
-      ...state,
-      calenderCheckOut: !state.calenderCheckOut,
-      calenderCheckin: false,
-    });
-  };
-
-  const onChange = (newValue: any) => {
-    setstate({
-      ...state,
-      checkIn: newValue,
-      calenderCheckOut: false,
-      checkOut: null,
-      calenderCheckin: false,
-    });
-  };
-  const onChangeout = (newValue: any) => {
-    setstate({
-      ...state,
-      checkOut: newValue,
-      calenderCheckin: false,
-      calenderCheckOut: !state.calenderCheckOut,
-    });
-  };
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => document.removeEventListener("mousedown", handleClickOutside);
-  });
-
-  useEffect(() => {
-    setstate({
-      ...state,
-      calenderCheckOut: false,
-      calenderCheckin: false,
-    });
-  }, [theme.navMobile]);
-
-  useEffect(() => {
-    searchBoxRef.current.focus();
-  }, [theme.focus]);
+  const {
+    handleClickout,
+    onChange,
+    onChangeout,
+    handleBlur,
+    handleClickOutside,
+    RefDet,
+    searchBoxRef,
+    state,
+    setstate,
+    handleChange,
+    handlefocus,
+    handleClickinside,
+    handleClickin,
+  } = useSearch();
 
   return (
     <div className="homesearch">
